5f934cae7d6831d523e88324ade7a9c5e3021cf6
Vasilev Mikhail <mall.87@mail.ru>
2023-07-24 17:58:29 +0300

    add list

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,14 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public class App {
+    public static boolean scrabble(String str, String word){
+        return true;
+    }
+}
+//END
+
