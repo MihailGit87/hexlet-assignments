ac72485b9337a8e3de60797f3e96af1d84952c39
Vasilev Mikhail <mall.87@mail.ru>
2023-07-29 15:36:24 +0300

    add list decision

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -6,9 +6,30 @@ import java.util.List;
 
 // BEGIN
 public class App {
-    public static boolean scrabble(String str, String word){
-        return true;
+    public static boolean scrabble(String str, String word) {
+
+        if (str.length() < word.length()) {
+            return false;
+        }
+
+        String[] strArr = str.split("");
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            list.add(word.substring(i,i + 1).toLowerCase());
+        }
+
+        for (int i = 0; i < strArr.length; i++) {
+            if (list.contains(strArr[i])){
+                list.remove(strArr[i]);
+            }
+        }
+
+        if (list.size() == 0) {
+            return true;
+        }
+
+        return false;
     }
 }
 //END
-
