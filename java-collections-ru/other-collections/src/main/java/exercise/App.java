ca6e1c5c13b49058b9588ed0a3734afff8886048
mikhailvasilev <mall.87@mail.ru>
2023-08-21 10:21:30 +0300

    add solution

---
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -0,0 +1,14 @@
+package exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+// BEGIN
+class App{
+    public LinkedHashMap<String, String> genDiff (Map<String, Object> data1, Map<String, Object> data2) {
+
+    }
+}
+//END
