d69f7d04f1e38564b7ac20083f9cb46940f14c0a
mikhailvasilev <mall.87@mail.ru>
2023-08-24 17:13:16 +0300

    add solution

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -4,11 +4,36 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.function.Function;
 
 // BEGIN
-class App{
-    public LinkedHashMap<String, String> genDiff (Map<String, Object> data1, Map<String, Object> data2) {
+class App {
+    public static LinkedHashMap<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+        TreeSet<String> keys = new TreeSet<>();
+        keys.addAll(data1.keySet());
+        keys.addAll(data2.keySet());
 
+        Function<String, Map<String, String>> comparator = (key) -> {
+            if (!data1.containsKey(key)) {
+                return Map.of(key, "added");
+            }
+            if (!data2.containsKey(key)) {
+                return Map.of(key, "deleted");
+            }
+            if (data1.get(key).equals(data2.get(key))) {
+                return Map.of(key, "unchanged");
+            }
+            return Map.of(key, "changed");
+        };
+
+        return keys.stream()
+                .map(comparator)
+                .collect(
+                        LinkedHashMap::new,
+                        LinkedHashMap::putAll,
+                        LinkedHashMap::putAll
+                );
     }
+
 }
 //END
