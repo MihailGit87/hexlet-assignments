0ddbdbc132f6cfaf7bcee0d7250cedd76c57d46b
mikhailvasilev <mall.87@mail.ru>
2023-10-04 14:01:43 +0300

    change solution

---
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -0,0 +1,13 @@
+package exercise;
+
+import java.util.Map;
+import java.util.List;
+
+// BEGIN
+public class PairedTag extends Tag  {
+
+    public PairedTag(String p, Map<String, String> attributes1, String textParagraph, List<Tag> tags) {
+
+    }
+}
+// END
--- java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -3,16 +3,16 @@ package exercise;
 // BEGIN
 public class LabelTag implements TagInterface {
     String tagText;
-    Object inputTag;
+    InputTag inputTag;
 
-    public LabelTag(String tagText, Object inputTag) {
+    public LabelTag(String tagText, TagInterface inputTag) {
         this.tagText = tagText;
-        this.inputTag = inputTag;
+        this.inputTag = (InputTag) inputTag;
     }
 
     @Override
     public String render() {
-        return "<label>" + tagText + "<input type=\"submit\" value=\"Save\"></label>";
+        return "<label>" + tagText + inputTag.render() + "</label>";
     }
 }
 // END
