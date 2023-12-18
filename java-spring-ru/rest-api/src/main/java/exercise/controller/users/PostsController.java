346de193d3e02725baedfefeeb2732c5bb77dd4b
@hexlet/cli <support@hexlet.io>
2023-12-18 20:33:38 +0300

    submit java-spring-ru/rest-api

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -15,24 +15,19 @@ import exercise.Data;
 public class PostsController {
     List<Post> posts = Data.getPosts();
 
-    @GetMapping("/users/{id}/posts")
-    public Optional<Post> show(@PathVariable String id) {
-        var post = posts.stream()
-                .filter(p -> p.getSlug().equals(id))
-                .findFirst();
-        return post;
+    @GetMapping("/users/{userId}/posts")
+    public List<Post> show(@PathVariable Integer userId) {
+        return posts.stream()
+                .filter(p -> p.getUserId() == userId)
+                .toList();
     }
 
-    @PostMapping("/users/{id}/posts")
+    @PostMapping("/users/{userId}/posts")
     @ResponseStatus(HttpStatus.CREATED)
-    public Post create(@RequestBody Post post) {
-        Post post1 = new Post();
-        post1.setUserId(post.getUserId());
-        post1.setSlug(post.getSlug());
-        post1.setBody(post.getBody());
-        post1.setTitle(post.getTitle());
-        posts.add(post1);
-        return post1;
+    public Post create(@PathVariable Integer userId, @RequestBody Post post) {
+        post.setUserId(userId);
+        posts.add(post);
+        return post;
     }
 }
 // END
